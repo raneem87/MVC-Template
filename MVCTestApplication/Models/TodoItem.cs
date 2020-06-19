7bd6e7ec393b491f27fed9ad7bb933e8e3d21907
raneem87 <raneem87@users.noreply.github.com>
2020-06-19 21:45:31 +0300

    controller- model- view model- view added

---
+++ MVCTestApplication/Models/TodoItem.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime;
+using System.Threading.Tasks;
+
+namespace MVCTestApplication.Models
+{
+    public class TodoItem // POCO Plan old c# object
+    {
+        public Guid Id { get; set; }
+        public bool IsDone { get; set; }
+        public string Title { get; set; }
+
+        public DateTimeOffset? DueAt { get; set; }
+        public List<TodoItem> MyProperty { get; set; }
+
+
+    }
+}
