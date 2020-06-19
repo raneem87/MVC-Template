ec5938ac5f5fe66d4fde028ed76386a1bfabf844
raneem87 <raneem87@users.noreply.github.com>
2020-06-20 02:26:16 +0300

    fake and real services added

--- MVCTestApplication/Startup.cs
+++ MVCTestApplication/Startup.cs
@@ -1,12 +1,10 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using MVCTestApplication.Models;
+using MVCTestApplication.Services;
 
 namespace MVCTestApplication
 {
@@ -22,7 +20,9 @@ namespace MVCTestApplication
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+
             services.AddControllersWithViews();
+            services.AddSingleton<ITodoItemService, FakeTodoItemService>();              
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
