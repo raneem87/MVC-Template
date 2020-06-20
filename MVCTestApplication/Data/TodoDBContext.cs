3337106306c289d49e66a3903f56db328a099d2e
raneem87 <raneem87@users.noreply.github.com>
2020-06-20 09:58:34 +0300

    efcore added, migration done

---
+++ MVCTestApplication/Data/TodoDBContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MVCTestApplication.Models;
+
+namespace MVCTestApplication.Data
+{
+    public class TodoDBContext:DbContext
+    {
+        public TodoDBContext(DbContextOptions<TodoDBContext> options) :base(options)
+        {            
+        }
+        public DbSet<TodoItem> TodoItems { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder )
+        {
+            base.OnConfiguring(optionsBuilder);
+        }
+    }
+}
